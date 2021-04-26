02bd9f9008e0902205171a0e2ae566a4a393d62f
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-26 12:12:43 +0300

    added consts in app

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// select items
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+
